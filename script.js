95fbbe3c75fc59a2582fcf0dd34e8401101c48d1
Dheeraj Gupta <djgupta820@gmail.com>
2023-05-18 12:58:45 +0530

    search box, cart list and about section added

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 let openMenu = document.querySelector('#openMenu')
-let toggle = true;
+let search = document.querySelector('#search')
+let cart = document.querySelector('#cart')
+
+let menuToggle = true;
+let searchToggle = true;
+let cartToggle = true;
 
 openMenu.addEventListener('click', ()=>{
     let links = document.getElementById('links')
-    if(toggle){
+    if(menuToggle){
         links.style.right = '0'
-        toggle = false
+        menuToggle = false
     }else{
         links.style.right = '-400px'
-        toggle = true    
+        menuToggle = true    
+    }
+})
+
+search.addEventListener('click', ()=>{
+    if(searchToggle){
+        document.querySelector('#query').style.display = 'block'
+        searchToggle = false
+    }else{
+        document.querySelector('#query').style.display = 'none'
+        searchToggle = true
+    }
+})
+
+cart.addEventListener('click', ()=>{
+    let items = document.querySelector('#cartItems')
+    if(cartToggle){
+        items.style.right = '0'
+        cartToggle = false
+    }else{
+        items.style.right = '-500px'
+        cartToggle = true
     }
 })
